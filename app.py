0546d8205d20420c9882c925a295500e875fbb71
Flabelatus <98832679+Flabelatus@users.noreply.github.com>
2022-05-29 22:32:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask
 from flask_jwt import JWT, timedelta  # For authentication
 from flask_restful import Api
@@ -11,12 +10,7 @@ from security import authenticate, identity
 
 
 app = Flask(__name__)
-uri = os.getenv("DATABASE_URL")  # or other relevant config var
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-# rest of connection code using the connection string `uri`
-
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL, 'sqlite:///user_database.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user_database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
