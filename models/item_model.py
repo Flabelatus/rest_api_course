dd957eee5198cbf99e5ba1b286bcec59f8216fed
jjooshesh <jooshesh.javid@gmail.com>
2022-05-30 15:18:03 +0200

    docStrings added for ItemModel

--- models/item_model.py
+++ models/item_model.py
@@ -2,7 +2,37 @@ from db import db
 
 
 class ItemModel(db.Model):
-    # The name of the table to be created in the db
+    """
+    The Item model class to include the main attributes and methods representing
+    the items in the Store application.
+
+        Attributes:
+             name [str]: The name of the item
+             price [float]: The price for each item
+             store_id [int]: The id of the store that the item belongs to. Mainly
+                for the database table reference
+
+        Methods:
+            json:
+                Create JSON object from the item
+                :return A dictionary containing the item name and the item price
+                    representing the item as JSON.
+
+            find_by_name:
+                Find the item by the `name` from the database.
+                :param `name` of the item to find from database
+                :return The item that was searched from the database
+
+            save_to_db
+                Add the item to the database and commits
+
+            delete_from_db
+                Deletes the item from the database and commits
+
+    The following is a set of code lines to add in order to set up the connection
+    between the SQLAlchemy and the Flask app. It is important to have the models
+    connected to the SQLAlchemy database in order to create the related tables.
+    """
     __tablename__ = 'items'
 
     # Adding the id, name and price in the rows in the db
@@ -10,7 +40,7 @@ class ItemModel(db.Model):
     name = db.Column(db.String(80))
     price = db.Column(db.Float(precision=2))
 
-    # For relational purpose to connect the table with the different table.
+    # For relational purpose to connect the table with the different table (Using Foreign Key).
     store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
     store = db.relationship('StoreModel')
 
