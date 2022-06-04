606e36b15a011033d215f3469f5e6d51306531b3
jjooshesh <jooshesh.javid@gmail.com>
2022-06-05 01:28:57 +0200

    DATABASE_URL

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from security import authenticate, identity
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL_2', 'sqlite:///user_database.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///user_database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
