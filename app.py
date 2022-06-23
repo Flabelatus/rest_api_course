4aa53487c3d80d8304db0f8e9b20fe899993ac03
Flabelatus <jooshesh.javid@gmail.com>
2022-06-24 01:20:00 +0200

    uri changed to postgresql://

--- app.py
+++ app.py
@@ -12,10 +12,7 @@ from security import authenticate, identity
 
 
 app = Flask(__name__)
-uri = os.getenv('DATABASE_URL')
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///user_database.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///user_database.db')
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
